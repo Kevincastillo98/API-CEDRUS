067e2ae13d117de038a12aafe0e5dd4c36cdd938
kevin <kcastillo@akval.com.mx>
2021-01-24 22:09:48 -0600

    Serializacion de archivos csv

--- Proyecto/flask-covid/aplicacion.py
+++ Proyecto/flask-covid/aplicacion.py
@@ -1,20 +1,46 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-import pandas as pd
-from flask import Flask, request, render_template, session, redirect
-
+from flask import Flask,render_template, request, send_file
+from flask import Flask, Response, render_template
+from flask_mysqldb import MySQL
+import io
+import csv
 
 
 app = Flask(__name__)
+ 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'kevin'
+app.config['MYSQL_PASSWORD'] = 'castilloroa'
+app.config['MYSQL_DB'] = 'ETL'
+
+mysql = MySQL(app)
 
 
-@app.route('/',methods=("GET","POST"))
-def index():
-    
-    df = pd.read_csv("/home/kevin/Documentos/API-CEDRUS/Proyecto/Datos/muestra_web.csv",index_col=0)
-    return render_template('simple.html',tables=[df.to_html(classes='data')],titles=df.columns.values)
+@app.route("/<estado>")
+def user(estado):
+    cur = mysql.connection.cursor() 
+    cur.execute("""SELECT *  FROM datos_covid_geo WHERE NOMBRE_ENTIDAD_RES = %s """, (estado,))
+    result = cur.fetchall()
+    output = io.StringIO()
+    writer = csv.writer(output)
+    column_names = [i[0] for i in cur.description]
+    writer.writerow(column_names)
+    writer.writerows(result)
+    output.seek(0)
+    return Response(output, mimetype="text/csv", headers={"Content-Disposition":"attachment;filename={}.csv".format(estado)})
+	
+@app.route("/<estado>/<municipio>")
+def busqueda(estado,municipio):
+    cur = mysql.connection.cursor() 
+    cur.execute("""SELECT * FROM datos_covid_geo WHERE NOMBRE_ENTIDAD_RES = %s AND NOMBRE_MUNICIPIO_RES = %s """, (estado,municipio))
+    result = cur.fetchall()
+    output = io.StringIO()
+    writer = csv.writer(output)
+    column_names = [i[0] for i in cur.description]
+    writer.writerow(column_names)
+    writer.writerows(result)
+    output.seek(0)
+    return Response(output, mimetype="text/csv", headers={"Content-Disposition":"attachment;filename={}-{}.csv".format(estado,municipio)})
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
+app.run(host='localhost', port=5000)
